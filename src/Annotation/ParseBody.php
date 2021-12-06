9b1c61f6b13c8c23bdb52ab5beb318ca2cab4fdc
4165306 <416306@q.com>
2021-12-06 17:11:27 +0800

    新增重写中间件，防止在AOP之前中间件对参数进行校验

--- src/Annotation/ParseBody.php
+++ src/Annotation/ParseBody.php
@@ -1,15 +1,26 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Caterpillar\HyperfAnnotationParseBody\Annotation;
 
 use Hyperf\Di\Annotation\AbstractAnnotation;
 
 /**
- * @author Caterpillar
- * Class ParseBody
- * @package Caterpillar\HyperfAnnotationParseBody\Annotation
  * @Target("ALL")
  * @Annotation
  */
 #[\Attribute(\Attribute::TARGET_ALL)]
-class ParseBody extends AbstractAnnotation {}
+class ParseBody extends AbstractAnnotation
+{
+    public function __construct($attribute)
+    {
+    }
+}
--- src/Middleware/ParseBodyCoreMiddleware.php
+++ src/Middleware/ParseBodyCoreMiddleware.php
@@ -1,9 +1,69 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Caterpillar\HyperfAnnotationParseBody\Middleware;
 
+use Hyperf\Di\ClosureDefinitionCollectorInterface;
+use Closure;
 
-class ParseBodyCoreMiddleware
+class ParseBodyCoreMiddleware extends \Hyperf\HttpServer\CoreMiddleware
 {
+    /**
+     * Parse the parameters of method definitions, and then bind the specified arguments or
+     * get the value from DI container, combine to a argument array that should be injected
+     * and return the array.
+     */
+    protected function parseMethodParameters(string $controller, string $action, array $arguments): array
+    {
+        $definitions = $this->getMethodDefinitionCollector()->getParameters($controller, $action);
+        return $this->getInjections($definitions, "{$controller}::{$action}", $arguments);
+    }
 
+    /**
+     * Parse the parameters of closure definitions, and then bind the specified arguments or
+     * get the value from DI container, combine to a argument array that should be injected
+     * and return the array.
+     */
+    protected function parseClosureParameters(Closure $closure, array $arguments): array
+    {
+        if (! $this->container->has(ClosureDefinitionCollectorInterface::class)) {
+            return [];
+        }
+        $definitions = $this->getClosureDefinitionCollector()->getParameters($closure);
+        return $this->getInjections($definitions, 'Closure', $arguments);
+    }
+
+    private function getInjections(array $definitions, string $callableName, array $arguments): array
+    {
+        $injections = [];
+        foreach ($definitions ?? [] as $pos => $definition) {
+            $value = $arguments[$pos] ?? $arguments[$definition->getMeta('name')] ?? null;
+            if ($value === null) {
+                if ($definition->getMeta('defaultValueAvailable')) {
+                    $injections[] = $definition->getMeta('defaultValue');
+                } elseif ($definition->allowsNull()) {
+                    $injections[] = null;
+                } elseif ($this->container->has($definition->getName())) {
+                    $injections[] = $this->container->get($definition->getName());
+                } else {
+                    $type = $definition->getName();
+                    var_dump($definition);
+                    $defaultValue = $definition->getMeta('defaultValue');
+                    settype($defaultValue, $type === 'int' ? 'integer' : $type);
+                    $injections[] = $defaultValue;
+                }
+            } else {
+                $injections[] = $this->getNormalizer()->denormalize($value, $definition->getName());
+            }
+        }
+        return $injections;
+    }
 }
